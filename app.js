142c6d02a558c02453fa8d256d9315e0afde5396
letskuku <yujung9288@naver.com>
2023-09-08 03:06:31 +0900

    feat: 글자 클릭 후 글자 색 변경 구현

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-alert("hi");
+const h1 = document.querySelector(".hello h1");
+
+function handleTitleClick() {
+    h1.classList.toggle("clicked");
+}
+
+h1.addEventListener("click", handleTitleClick);
