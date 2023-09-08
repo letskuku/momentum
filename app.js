b73d8a95cc0c41679fcb28e5937a8e65bdaecf9b
letskuku <yujung9288@naver.com>
2023-09-09 02:47:23 +0900

    feat: username 입력 폼 및 eventListener 구현

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-const h1 = document.querySelector(".hello h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-function handleTitleClick() {
-    h1.classList.toggle("clicked");
+// 모든 EventListener function의 첫번째 argument는 항상 지금 막 벌어진 일들에 대한 정보가 됨
+function onLoginSubmit(event) {
+    // preventDefault: 어떤 event의 기본 행동이든 발생되지 않도록 막음, EventListener 함수의 첫번째 argument 안에 있는 함수임
+    // ex) form의 경우 submit하면 브라우저는 기본적으로 페이지를 새로고침 하도록 되어있음
+    event.preventDefault();
+    console.log(event);
 }
 
-h1.addEventListener("click", handleTitleClick);
+loginForm.addEventListener("submit", onLoginSubmit);
